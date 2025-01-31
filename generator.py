0219402059831aebc147573306e673403c448dfc
Abena <priscillaabenaaboagye@gmail.com>
2025-01-31 13:04:33 +0000

    assigning variables

---
+++ generator.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+# All the characters for the password
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+
+# Combine all the characters
+all_characters = letters + digits + symbols
