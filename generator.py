7f4d662cf29f0f8caa73e1acd012071c5b4a5ef2
Abena <priscillaabenaaboagye@gmail.com>
2025-01-31 13:18:03 +0000

    importing secret instead of random

--- generator.py
+++ generator.py
@@ -1,10 +1,11 @@
-import random
+import secrets
 import string
 
-# All the characters for the password
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
+def generate_password():
+    # All the characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
 
-# Combine all the characters
-all_characters = letters + digits + symbols
+    # Combine all the characters
+    all_characters = letters + digits + symbols
